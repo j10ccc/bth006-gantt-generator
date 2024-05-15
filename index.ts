1dab8461fb69804e7688a2a3cf709bc84ea29bda
j10c <blyb1739@gmail.com>
2024-05-16 00:33:33 +0800

    feat(index): arrange tasks concurrently

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
 import xlsx from 'node-xlsx';
 
 interface Task {
-  phase: "Analysis" | "Design" | "Development" | "Test Planning" | "Test Execution"
-  name: `${string}_${string}`;
+  anchor: string;
+  phase: "Analysis" | "Design" | "Development" | "Test Planning" | "Test Execution";
+  name: string;
   country: "BJ" | "ID" | "SE" | "KR" | "CZ";
   personMonth: number;
   time: number; // days(ceil int)
 }
 
 const sheets = xlsx.parse(process.argv[2]);
-
-console.log(process.argv);
 
 const data = sheets.find(sheet => sheet.name === "修订版3.0")!.data
 
@@ -28,8 +27,9 @@ for (let i = 0 + 3; i < 30 + 3; i++) {
 
     if (count) {
       tasks.push({
+        anchor: data[i][0],
         phase,
-        name: `${data[i][0]}_${data[i][1]}`,
+        name: data[i][1],
         country: data[2][j],
         personMonth: count,
         time: peopleCount ? Math.ceil(count / data[42][j] * 31) : -1
@@ -41,12 +41,23 @@ for (let i = 0 + 3; i < 30 + 3; i++) {
 const phases: Task["phase"][] = ["Analysis", "Design", "Development", "Test Planning", "Test Execution"];
 
 const sections = phases.map(phase => {
+  const progress = new Map<Task["country"], string>();
   const startDate = "2024-01-01";
   let section = ""
   const tmp = tasks.filter(task => task.phase === phase);
-  tmp.forEach((task, index) => {
-    section = section + `
-      ${task.name} :${index === 0 ? startDate + ", " : ""}${task.time}d`
+
+  tmp.forEach(task => {
+    const previousWork = progress.get(task.country);
+    const taskId = (task.phase + "_" + task.anchor.split(".").join("")).split(" ").join("");
+
+    if (previousWork) {
+      section += `
+        [${task.country}]${task.name} :${taskId}, after ${previousWork}, ${task.time}d`;
+    } else {
+      section += `
+        [${task.country}]${task.name} :${taskId}, ${startDate}, ${task.time}d`;
+    }
+    progress.set(task.country, taskId);
   })
 
   return section;
@@ -56,16 +67,11 @@ const gantt = `gantt
     title A Gantt Diagram
     dateFormat YYYY-MM-DD
     excludes weekends
-    section Analysis
-${sections[0]}
-    section Design
-${sections[1]}
-    section Development
-${sections[2]}
-    section Test Planning
-${sections[3]}
-    section Test Execution
-${sections[4]}
+    section Analysis ${sections[0]}
+    section Design ${sections[1]}
+    section Development ${sections[2]}
+    section Test Planning ${sections[3]}
+    section Test Execution ${sections[4]}
 `;
 
 console.log(gantt);
